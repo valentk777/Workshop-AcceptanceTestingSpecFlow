310fac21188732fb392a64b6171d4967269bfe20
Ilja Dmitrijevs <ilja.dmitrijevs@accenture.com>
2023-02-21 11:35:04 +0200

    New tests project

---
+++ TreeDelivaryApp.Tests/Steps/TreeOrderSteps.cs
@@ -0,0 +1,39 @@
+﻿
+using TechTalk.SpecFlow;
+
+namespace TreeDelivaryApp.Tests.Steps 
+{
+    [Binding]
+    public class GetTreeSteps 
+    {
+        [Given(@"enter valid tree name (.*)")]
+        public void GivenValidTreeName(string treeName) 
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [When(@"press the button (.*)")]
+        public void WhenMakeOrderButtonIsPressed(string buttonName)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then(@"ordered succesfully")]
+        public void OrderSuccess()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"enter invalid tree name (.*)")]
+        public void GivenInvalidTreeName(string treeName)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then(@"order fails")]
+        public void OrderFail()
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
